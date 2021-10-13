e8a315fa21d478ee8ed0d580473a2163dd3a7e21
beetogaspar <gilberto.sgaspar@gmail.com>
2021-10-12 22:24:24 -0300

    adicionado exercicios da aula

---
+++ exercicios.js
@@ -0,0 +1,43 @@
+//const base = 5;
+//let height = 8;
+
+
+
+//console.log(base * height);
+// if -se
+// else - senão
+
+//-------------------------------------------------------------------
+
+
+//let nota = 0;
+
+//if (nota >= 80) {
+//  console.log("Parabens você foi aprovado");
+//} else if (nota < 80 && nota >= 60) {
+//  console.log("voce está na nossa lista de espera");
+
+//} el/se {
+//  console.log("você foi reprovado");
+//}
+
+let resultadoProcesso = "lista";
+
+switch (resultadoProcesso) {
+    case "aprovado":
+        console.log("Voce foi aprovado");
+        break;
+
+    case "reprovado":
+        console.log("reprovado");
+        break;
+
+    case "lista":
+        console.log("voce está na lista de espera");
+        break;
+
+    default:
+        console.log("Não se aplica");
+}
+
+//------------------------------------------------
